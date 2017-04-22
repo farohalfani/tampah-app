26f498f731c5c9906f9237802f8af6309370b7c0
farohalfani <farohnuralfani@gmail.com>
2017-04-22 23:51:58 +0700

    action bar

--- app/src/main/java/com/android/tampah_app/MainActivity.java
+++ app/src/main/java/com/android/tampah_app/MainActivity.java
@@ -8,6 +8,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.logo);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
         setContentView(R.layout.activity_main);
     }
 }
