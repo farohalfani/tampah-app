70884e4c3b725b75bbdd8b926b3709f84a60b588
pramestisp <shindy.putri@yahoo.co.id>
2017-05-01 10:32:59 +0700

    invoice, ketsewa, paymentconfirm done 2

--- app/src/main/java/com/android/tampah_app/LoginActivity.java
+++ app/src/main/java/com/android/tampah_app/LoginActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -29,6 +30,21 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
                 LoginActivity.this.startActivity(registerIntent);
+            }
+        });
+
+        bLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!etEmail.getText().toString().isEmpty() && !etPassword.getText().toString().isEmpty()) {
+                    Toast.makeText(LoginActivity.this,
+                            "Login berhasil",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(LoginActivity.this,
+                            "Kolom yang Anda isikan tidak lengkap",
+                            Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
